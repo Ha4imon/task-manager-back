82ad97d01aca68f3480467a1546dddb8615fb9dd
Ha4imon <v1227873@yandex.ru>
2024-06-15 00:41:20 +0400

    fix cors

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class MainController {
   private final TaskRepo taskRepo;
   private final ObjectMapper objectMapper;
